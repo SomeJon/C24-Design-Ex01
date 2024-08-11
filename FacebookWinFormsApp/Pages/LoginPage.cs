54d2ba91c10d44daffa68b72921666c9a805e93c
Jonathan Shaya <jonathansh2@mta.ac.il>
2024-08-11 20:51:46 +0300

    -Started with pages as controls being pasted on top of a tab -Bug fixes

---
+++ FacebookWinFormsApp/Pages/LoginPage.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace FacebookClient.New_Buttons
+{
+    public partial class LoginPage : UserControl, PageControl
+    {
+        public event EventHandler SelectedChoice;
+
+        public override Color BackColor {  get; set; }
+
+        public LoginPage()
+        {
+            InitializeComponent();
+        }
+
+        private void loginButton_Click(object sender, EventArgs e)
+        {
+            if (SelectedChoice != null)
+            {
+                SelectedChoice.Invoke(sender, EventArgs.Empty);
+            }
+        }
+
+        private void settingButton_Click(object sender, EventArgs e)
+        {
+            if (SelectedChoice != null)
+            {
+                SelectedChoice.Invoke(sender, EventArgs.Empty);
+            }
+        }
+    }
+}
