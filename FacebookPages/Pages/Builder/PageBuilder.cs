990a037018b442eb34a644d39ba5f4ba5e2e07db
Jonathan Shaya <jonathansh2@mta.ac.il>
2024-08-15 14:26:38 +0300

    -Started the Builder design for page -Opened class theme -Added an interface PageData for the diffrent Page Data that would be loaded

---
+++ FacebookPages/Pages/Builder/PageBuilder.cs
@@ -0,0 +1,17 @@
+﻿using FacebookClient.Pages;
+using FacebookPages.Pages.Data;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FacebookPages.Pages.Builder
+{
+    public interface PageBuilder
+    {
+        PageBuilder AddData(PageData i_PageData);
+        PageBuilder AddTheme(Theme i_Theme);
+        BasePage Build();
+    }
+}
--- FacebookPages/Pages/Data/HomePageData.cs
+++ FacebookPages/Pages/Data/HomePageData.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using FacebookPages.Pages.Data;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -6,7 +7,7 @@ using System.Threading.Tasks;
 
 namespace FacebookClient.Code.Pages.Data
 {
-    public class HomePageData
+    public class HomePageData : PageData
     {
         public string ProfilePicUrl { get; set; }
         public string UserName { get; set; }
