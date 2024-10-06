43929ada61ef324eaf9ef44732f3a5c2dd0a8f93
Jonathan Shaya <jonathansh2@mta.ac.il>
2024-10-06 16:29:02 +0300

    code cleaning

--- FactoryHelper/SortingOptions.cs
+++
@@ -1,9 +0,0 @@
-﻿using System.Collections.Generic;
-
-namespace FacebookWrapperEnhancements.Code.Collection.Sort
-{
-    public static class StrategyOptions<T> where 
-    {
-        public Dictionary<e>
-    }
-}
